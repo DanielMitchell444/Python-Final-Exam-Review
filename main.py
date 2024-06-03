070898b5941beabc667187b6404eba17b6765a4a
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-06-03 10:35:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,227 @@
+'''
+Please read the questions carefully and answer the questions to
+the best of your ability
+'''
+
+'''
+Points Possible: /2
+QUESTION 1A:
+Below this comment, i want you to create 3 variables, one
+variable will be a boolean, one will be a number, and
+one will be a string
+'''
+
+'''
+QUESTION 1B
+Explain what a boolean, a string, and an integer are
+String:
+Integer:
+Boolean:
+'''
+
+'''
+Points Possible: /2
+QUESTION 1C: 
+Below this comment are variables that are equal to 2 and 1. They should
+be named numberUno and numberDos.
+I want you to create a variable called sum. Inside this variable, i want
+you to add numberUno and numberDos together. After that, print out
+the sum variable.
+'''
+
+'''
+Points Possible: /2
+QUESTION 1D:
+Below this comment is a string inside the variable named name.
+Below this comment, i want you to select the first 2 characters of the
+string and print them out using the .slice method
+HINT: Strings start at index 0, and with the slice method, the last
+argument is not included.
+'''
+name = "Mario"
+
+'''
+QUESTION 1E:
+Points Possible: /2
+Below this comment will be two string variables. One will be called a,
+and the other will be called b
+Below this comment, and the variables, i want you to create a variable called
+c that adds the variables a and b together, then, i want you to print out
+the variable c.
+'''
+a = "Mario"
+b = "Kart"
+
+
+
+'''
+QUESTION 1F:
+Points Possible: /2
+Below this comment, i want you to use a for loop to loop through
+the string name. Then, i want you to print out the string
+'''
+
+'''
+QUESTION 1G:
+Points Possible: /2
+Below this comment, you will see the variable m that is equal to redbull.
+Below this comment and the variable, i want you to change the string to
+uppercase letters using .upper()
+'''
+m = "redbull"
+'''
+QUESTION 1H:
+Points Possible: /2
+Now using the same variable as the question above, 
+change the string variable to lowercase using .lower()
+'''
+
+'''
+Points Possible: /2
+QUESTION 2:
+Below this comment, create a list with 3 items inside it. You can name
+the list whatever you want
+'''
+
+
+
+'''
+Points Possible: /2
+QUESTION 2A:
+Using the list you just created, add the word mario to the end
+of the list
+HINT: to add an item in python, we use .append()
+'''
+
+'''
+Points Possible: /2
+QUESTION 2B:
+Using the list you created, i want you to remove the word mario
+from the list using the .remove() method.
+Post the code below this comment
+'''
+
+'''
+Points Possible: /2
+QUESTION 2C:
+Using the list above, i want you to change the 1st index
+of the list to luigi. Please post the code below this comment.
+'''
+
+'''
+QUESTION 2D:
+Points Possible: /2
+What will be the output of this code?
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+'''
+
+'''
+QUESTION 2E:
+Points Possible: /2
+Below this comment, i want you to create another list with 3 items inside
+it
+After that, i want you to use the .sort() method to sort the list
+from ascending to decending.
+'''
+
+'''
+QUESTION 2F:
+Points Possible: /2
+Using the list you created above, use the .reverse() method to reverse the list.
+'''
+
+'''
+Points Possible: /2
+QUESTION 3:
+Below, you will see a variable named score with the value of 2.
+Below this comment, i want you to make a if elif statement
+For the if statement, i want you to check to see if the value is
+less than 1. If it is, print out this is incorrect.
+Inside the elif statement, i want you to check to see if score is
+equal to 2, if it is, then print out this is correct.
+The last statement should be an else statement that prints out
+none of these are right.
+Please post the code below this comment.
+'''
+score = 2
+
+'''
+Points Possible: /2
+QUESTION 4a:
+Below this comment, i want you to create a list with the variable name
+of inventory. Inside, i want you to include 3 items of your choosing.
+Please post the code below this comment
+'''
+
+'''
+Points Possible: /2
+QUESTION 4B:
+Below this comment, i want you to use a for loop to loop through the list.
+Inside the for loop, i wnat you to print out each individual list item.
+Then at the end, i want you to include a break statement.
+'''
+
+'''
+Points Possible: /2
+QUESTION 4C:
+Below this comment, you will see two lists. One titled adj and one
+titled fruits. I want you to use a nested for loop to loop through both
+of the lists, then, i want you to print the two values.
+
+'''
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+'''
+Points Possible: /2
+QUESTION 4D:
+Below this comment, you will see a variable called inventory that is equal to
+0. 
+I want you to create a while loop that checks to see if inventory is less than 5,
+if it is, then increment the value until it is equal to 5.
+HINT: To increment a number, you will go +=1.
+So essentially, you want to go inventory+= 1
+'''
+inventory = 0
+
+'''
+Points Possible: /2
+QUESTION 4E:
+Explain what the while loop does inside this comment.
+'''
+
+
+'''
+Points Possible: /2
+QUESTION 5A:
+Below this comment, i want you to create a function called add that passes
+2 parameters, numberOne, and numberTwo inside it. Then, i want you to
+return the sum of the two numbers.
+Please post the code below this comment.
+'''
+
+'''
+Points Possible: /2
+QUESTION 5B:
+Below this comment, i want you to call the function and pass
+two numbers inside the function arguement.
+'''
+
+'''
+Points Possible: /2
+QUESTION 5C:
+What will be the output?
+'''
+
+'''
+
+'''
+
+
+'''
+FINAL GRADE: /46
+'''
